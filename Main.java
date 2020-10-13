c33848d297708fc0e78577e462e75ba3873c2512
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-13 15:49:29 +0000

    finished methods example

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Methonds example
+ * @author Jaden Ramcharan
  */
 public class Main {
+  //calculates area of rectangle given length and width
+  public static double areaOfRect(double length, double width){
+    //calculates area of rectangle
+    double area = length * width;
+    //returns area of the rectangle
+    return area;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    double l = input.nextDouble();
+    double w = input.nextDouble();
+    double area = areaOfRect(l, w);
+    System.out.println(area);
     
   }
 }
